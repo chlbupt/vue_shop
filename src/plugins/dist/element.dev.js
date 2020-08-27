bfd838a0329bd7c02414a8a75b66cb69a06011ae
chl <1032326058@qq.com>
2020-08-27 18:39:04 +0800

    '登录布局样式'

---
+++ src/plugins/dist/element.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var _vue = _interopRequireDefault(require("vue"));
+
+var _elementUi = require("element-ui");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_vue["default"].use(_elementUi.Button);
+
+_vue["default"].use(_elementUi.Form);
+
+_vue["default"].use(_elementUi.FormItem);
+
+_vue["default"].use(_elementUi.Input);
