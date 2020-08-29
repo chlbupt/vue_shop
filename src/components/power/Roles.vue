47b33bb93ddae8f192ac0d7ded499bbe0059f861
chl <1032326058@qq.com>
2020-08-29 23:36:44 +0800

    '渲染角色列表'

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -1,15 +1,64 @@
 <template>
     <div>
-        <h1>角色列表</h1>
+        <!-- 面包屑导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+        <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- 卡片视图 -->
+        <el-card>
+            <!-- 添加角色 -->
+            <el-row>
+                <el-col>
+                    <el-button type="primary">
+                        添加角色
+                    </el-button>
+                </el-col>
+            </el-row>
+            <!-- 角色列表区域 -->
+            <el-table :data="roleList" border stripe>
+                <el-table-column type="expand"></el-table-column>
+                <el-table-column type="index"></el-table-column>
+                <el-table-column label="角色名称" prop="roleName"></el-table-column>
+                <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
+                <el-table-column label="操作" width="300px">
+                    <template slot-scope="scope">
+                        <el-button type="primary" size="mini" icon="el-icon-edit">编辑</el-button>
+                        <el-button type="danger" size="mini" icon="el-icon-delete">删除</el-button>
+                        <el-button type="warning" size="mini" icon="el-icon-setting">分配权限</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </el-card>
     </div>
 </template>
 
 <script>
 export default {
+    created(){
+        this.getRoleList()
+    },
+    data(){
+        return {
+            roleList:[]
 
+        }
+    },
+    methods:{
+        async getRoleList(){
+            const {data: res} = await this.$http.get('roles')
+
+            if(res.meta.status != 200){
+                return this.$message.error('获取角色列表失败')
+            }
+
+            this.roleList = res.data
+            console.log(this.roleList)
+        }
+    }
 }
 </script>
 
 <style lang="less" scoped>
-
 </style>
