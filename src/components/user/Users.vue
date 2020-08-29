459ca9ef0a4cf3704371f232fc33c47e80b7e631
chlbupt <1032326058@qq.com>
2020-08-29 18:49:12 +0800

    1.添加搜索;2.添加用户表单

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -1,61 +1,88 @@
 <template>
-    <div>
-        <!-- 面包屑导航 -->
-        <el-breadcrumb separator-class="el-icon-arrow-right">
-            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
-            <el-breadcrumb-item>用户管理</el-breadcrumb-item>
-            <el-breadcrumb-item>用户列表</el-breadcrumb-item>
-        </el-breadcrumb>
-        <!-- 卡片视图 -->
-        <el-card>
-            <!-- 搜索区域 -->
-            <el-row :gutter="20">
-                <el-col :span="7">
-                    <el-input placeholder="请输入内容">
-                        <el-button slot="append" icon="el-icon-search"></el-button>
-                    </el-input>
-                </el-col>
-                <el-col :span="4">
-                    <el-button type="primary">添加用户</el-button>
-                </el-col>
-            </el-row>
-            <!-- 用户列表区域 -->
-            <el-table :data="userList" border stripe>
-                <el-table-column type="index"></el-table-column>
-                <el-table-column label="姓名" prop="username"></el-table-column>
-                <el-table-column label="邮箱" prop="email"></el-table-column>
-                <el-table-column label="电话" prop="mobile"></el-table-column>
-                <el-table-column label="角色" prop="role_name"></el-table-column>
-                <el-table-column label="状态">
-                    <template slot-scope="scope">
-                        <el-switch @change="userStateChanged(scope.row)"
-                            v-model="scope.row.mg_state">
-                        </el-switch>
-                    </template>
-                </el-table-column>
-                <el-table-column label="操作" width="180px">
-                    <template slot-scope="scope">
-                        <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
-                        <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
-                        <el-tooltip class="item" effect="dark" content="分配权限" placement="top" :enterable="false">
-                            <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
-                        </el-tooltip>
-                       
-                    </template>
-                </el-table-column>
-            </el-table>
-            <!-- 分页区域 -->
-            <el-pagination
-                @size-change="handleSizeChange"
-                @current-change="handleCurrentChange"
-                :current-page="queryInfo.pagenum"
-                :page-sizes="[1, 2, 3, 5]"
-                :page-size="queryInfo.pagesize"
-                layout="total, sizes, prev, pager, next, jumper"
-                :total="total">
-            </el-pagination>
-        </el-card>
-    </div>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图 -->
+    <el-card>
+      <!-- 搜索区域 -->
+      <el-row :gutter="20">
+        <el-col :span="7">
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getUserList">
+            <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" @click="dialogVisible = true">添加用户</el-button>
+        </el-col>
+      </el-row>
+      <!-- 用户列表区域 -->
+      <el-table :data="userList" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="姓名" prop="username"></el-table-column>
+        <el-table-column label="邮箱" prop="email"></el-table-column>
+        <el-table-column label="电话" prop="mobile"></el-table-column>
+        <el-table-column label="角色" prop="role_name"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-switch @change="userStateChanged(scope.row)" v-model="scope.row.mg_state"></el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="180px">
+          <template slot-scope="scope">
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            <el-tooltip
+              class="item"
+              effect="dark"
+              content="分配权限"
+              placement="top"
+              :enterable="false"
+            >
+              <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页区域 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1, 2, 3, 5]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
+    </el-card>
+
+    <!-- 模态框 -->
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="50%">
+      <!-- 内容主体区域 -->
+      <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="addForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <!-- 底部区域 -->
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -65,19 +92,70 @@ export default {
   },
   data() {
     return {
+      query: '',
       queryInfo: {
         query: '',
         pagenum: 1,
-        pagesize: 20
+        pagesize: 20,
       },
       userList: [],
-      total: 0
+      total: 0,
+      dialogVisible: false,
+      addForm: {
+        username:'',
+        password:'',
+        mobile:'',
+        email:'',
+      },
+      addFormRules: {
+        username: [
+          {
+            required: true,
+            message: '请输入用户名',
+            trigger: 'blur',
+          },
+          {
+            max: 10,
+            min: 3,
+            message: '用户名的长度在3~10个字符之间',
+            trigger: 'blur',
+          },
+        ],
+         password: [
+          {
+            required: true,
+            message: '请输入密码',
+            trigger: 'blur',
+          },
+          {
+            max: 15,
+            min: 6,
+            message: '密码的长度在6~15个字符之间',
+            trigger: 'blur',
+          },
+        ],
+         email: [
+          {
+            required: true,
+            message: '请输入邮箱',
+            trigger: 'blur',
+          }
+        ],
+         mobile: [
+          {
+            required: true,
+            message: '请输入手机',
+            trigger: 'blur',
+          },
+          
+        ],
+      },
     }
   },
   methods: {
     async getUserList() {
       const { data: res } = await this.$http.get('users', {
-        params: this.queryInfo
+        params: this.queryInfo,
       })
       if (res.meta.status != 200) {
         return this.$message.error(res.meta.msg)
@@ -86,25 +164,30 @@ export default {
       this.userList = res.data.users
       this.total = res.data.total
     },
-    handleSizeChange(newSize){
-        this.queryInfo.pagesize = newSize
-        this.getUserList()
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getUserList()
     },
-    handleCurrentChange(newPage){
-        this.queryInfo.pagenum = newPage
-        this.getUserList()
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getUserList()
     },
-    async userStateChanged(userInfo){
-        const {data: res} = await this.$http.put(`/users/${userInfo.id}/state/${userInfo.mg_state}`)
+    async userStateChanged(userInfo) {
+      const { data: res } = await this.$http.put(
+        `/users/${userInfo.id}/state/${userInfo.mg_state}`
+      )
 
-        if(res.meta.status != 200){
-            userInfo.mg_state = !userInfo.mg_state
-            return this.$message.error('更新用户状态失败')
-        }
+      if (res.meta.status != 200) {
+        userInfo.mg_state = !userInfo.mg_state
+        return this.$message.error('更新用户状态失败')
+      }
 
-        return this.$message.success('更新用户状态成功')
+      return this.$message.success('更新用户状态成功')
     },
-  }
+    change() {
+      self.$forceUpdate()
+    },
+  },
 }
 </script>
 
---
+++ src/components/user/test.vue
@@ -0,0 +1,11 @@
+this.message.success('111');
+this.message.success('111');
+this.message.success('res.meta.msg');
+this.message.success(res.meta.msg);
+
+{ required: true, message:, trigger: 'blur'}
+
+{ required: true, message:, trigger: 'blur'}
+{ required: true, message:'hell', trigger: 'blur'}
+{ required: true, message:'heel', trigger: 'blur'}
+this.message.success(res.meta.msg);
